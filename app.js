e02e89989882779c3c5df5c114cb74e9b29e20d3
okumu87 <okumu793@gmail.com>
2025-05-26 20:21:31 +0200

    active

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const socialBtn = document.querySelector(".btn-social");
 
+const socials = document.querySelector(".socials");
+
+socialBtn.addEventListener("click", () => {
+  socials.classList.toggle("hide");
+});
